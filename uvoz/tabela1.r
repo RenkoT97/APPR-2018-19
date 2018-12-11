cc64fc496710d49adc58d1f5cc9a8879c9809d4e
RenkoT97 <tjasa.renko@gmail.com>
2018-12-11 19:11:37 +0100

    Add files via upload
    program za prvo tabelo

---
+++ uvoz/tabela1.r
@@ -0,0 +1,23 @@
+library(readr)
+library(reshape2)
+library(dplyr)
+
+emisijeCO2 <- read_csv("emisijeCO2.csv", na = c(":"))
+emisijeCO2 <- emisijeCO2[-c(1:10),]
+emisijeCO2 <- emisijeCO2[,-c(3,4,5,7)]
+names(emisijeCO2) <- (c("LETO", "DRŽAVA", "EMISIJE OGLJIKOVEGA DIOKSIDA V TONAH"))
+
+emisijeCH4 <- read_csv("emisijeCH4.csv", na = c(":"))
+emisijeCH4 <- emisijeCH4[-c(1:10),]
+emisijeCH4 <- emisijeCH4[,-c(3,4,5,7)]
+names(emisijeCH4) <- (c("LETO", "DRŽAVA", "EMISIJE METANA V TONAH"))
+
+emisijeNO <- read_csv("emisijeNO.csv", na = c(":"))
+emisijeNO <- emisijeNO[-c(1:10),]
+emisijeNO <- emisijeNO[,-c(3,4,5,7)]
+names(emisijeNO) <- (c("LETO", "DRŽAVA", "EMISIJE DUŠIKOVEGA OKSIDA V TONAH"))
+
+tabela1 <- merge(emisijeCO2, emisijeCH4)
+tabela1 <- merge(tabela1, emisijeNO)
+tabela1$"EMISIJE TOPLOGREDNIH PLINOV V TONAH" <- tabela1$"EMISIJE OGLJIKOVEGA DIOKSIDA V TONAH" +
+tabela1$"EMISIJE METANA V TONAH" + tabela1$"EMISIJE DUŠIKOVEGA OKSIDA V TONAH"
