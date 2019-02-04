7672de492f720733788ffbfa35f4dfa2d4820656
Tjaša Renko <tjasa.renko@gmail.com>
2019-02-04 20:32:42 +0100

    Dodan zaključek

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,19 +1,23 @@
 library("factoextra")
 source("uvoz/tabela4.r", encoding="UTF-8")
+source("uvoz/tabela1.r", encoding="UTF-8")
 
 podatki <- na.omit(tabela4)
-podatki$"DELEŽ GDP" <- podatki$`DELEZ BDP, NAMENJEN VAROVANJU OKOLJA V OKVIRU JAVNEGA SEKTORJA` +
+podatki$"DELEÅ½ GDP" <- podatki$`DELEZ BDP, NAMENJEN VAROVANJU OKOLJA V OKVIRU JAVNEGA SEKTORJA` +
   podatki$`DELEZ BDP, NAMENJEN VAROVANJU OKOLJA V OKVIRU INDUSTRIJE` +
   podatki$`DELEZ BDP, NAMENJEN INVESTICIJAM ZA VAROVANJE OKOLJA, PORABLJENIM V JAVNEM SEKTORJU` +
   podatki$`DELEZ BDP, NAMENJEN INVESTICIJAM ZA VAROVANJE OKOLJA, PORABLJENIM V INDUSTRIJI`
 podatki <- na.omit(podatki)[-c(3,4,5,6)]
-podatki$"DELEŽ GDP" <- 10000 * podatki$"DELEŽ GDP"
+podatki$"DELEÅ½ GDP" <- 10000 * podatki$"DELEÅ½ GDP"
 podatki$vrstice <- paste(podatki$DRZAVA, podatki$LETO, sep =", ")
 rownames(podatki) <- podatki[,4]
 podatki <- podatki[-c(1,4)]
 
 fviz_nbclust(podatki, kmeans, method = "gap_stat")
 neki <- kmeans(podatki, 3)
-graf4 <- fviz_cluster(neki, data = podatki, main = "DELEŽ GDP, NAMENJEN VAROVANJU OKOLJA IN INVESTICIJAM VANJ",
+graf4 <- fviz_cluster(neki, data = podatki, main = "DELEÅ½ GDP, NAMENJEN VAROVANJU OKOLJA IN INVESTICIJAM VANJ",
              ellipse.type = "convex",
              ggtheme = theme_minimal())
+
+tabela <- na.omit(tabela1)
+graf5 <- ggplot(tabela) + aes(x=LETO, y=`EMISIJE TOPLOGREDNIH PLINOV V TONAH`) + geom_point() + geom_smooth(method = "lm")
